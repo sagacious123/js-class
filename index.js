425851fa87319111a503d3faaed4acd6c34c8803
sagacious123 <damilolaj23@gmail.com>
2022-12-12 18:46:52 +0100

    second push

--- index.js
+++ index.js
@@ -5,6 +5,44 @@ const list = document.querySelector(".list");
 const li = document.querySelectorAll("li");
 const input = document.querySelector("input");
 const nav = document.querySelector("nav");
+const btn3 = document.querySelector(".btn3");
+const colorInput = document.querySelector("#color");
+const colorInput2 = document.querySelector("#color2");
+const body = document.querySelector("body");
+
+const allColorInputs = document.querySelectorAll(".color");
+
+let color1;
+let color2;
+// function changeBackground() {
+//     colorInput.addEventListener("change", (e) => {
+//         color1 = e.target.value;
+//         body.style.background = "linear-gradient(" + color1 + "," + " " + color2 + ")";
+//         body.style.backgroundRepeat = "no-repeat";
+//     })
+
+//     colorInput2.addEventListener("change", (e) => {
+//         color2 = e.target.value;
+        // body.style.background = "linear-gradient(" + color1 + "," + " " + color2 + ")";
+//         body.style.backgroundRepeat = "no-repeat";
+//     })
+// }
+
+// changeBackground();
+
+function changeBackground() {
+  allColorInputs.forEach((input) => {
+    input.addEventListener("change", (e) => {
+      color1 = allColorInputs[0].value;
+      color2 = allColorInputs[1].value;
+      body.style.background = `linear-gradient(${color1}, ${color2})`;
+      body.style.backgroundRepeat = "no-repeat";
+      body.style.color = "white";
+    });
+  });
+}
+
+changeBackground();
 
 // btn.onclick = function () {
 //     para.textContent = "New paragragh content!"
@@ -25,7 +63,6 @@ const nav = document.querySelector("nav");
 //     // }
 
 //     nav.classList.toggle("open-nav");
-    
 
 //     // para.textContent = "New paragragh content!"
 //     //   li[0].textContent = "How are you?";
@@ -37,13 +74,13 @@ addBtn.addEventListener("click", add);
 // removeBtn.addEventListener("click", remove);
 
 function add() {
-    let value = input.value;
-    let li = document.createElement("li");
-    li.textContent = value;
-    li.classList.add("li");
-    list.appendChild(li);
-    input.value = "";
-    console.log(li)
+  let value = input.value;
+  let li = document.createElement("li");
+  li.textContent = value;
+  li.classList.add("li");
+  list.appendChild(li);
+  input.value = "";
+  console.log(li);
 }
 
 // function remove() {
@@ -55,10 +92,12 @@ function add() {
 //     console.log(l)
 // }
 
-addBtn.addEventListener("click", function() {
-    nav.classList.toggle("open-nav");
-})
+addBtn.addEventListener("click", function () {
+  nav.classList.toggle("open-nav");
+});
 
+btn3.addEventListener("click", () => {
+  alert("Clicked!!!");
+});
 
-
-console.log(nav.classList)
+console.log(nav.classList);
